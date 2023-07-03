a9a35d9b3a809c30a20bef71f6570b07b1b07e9a
junghee <junghee@nhn.com>
2023-07-03 19:57:13 +0900

    DI 패턴 예제

--- app/src/test/java/com/jhlee/cleanarchitecture/ExampleUnitTest.kt
+++ app/src/test/java/com/jhlee/cleanarchitecture/ExampleUnitTest.kt
@@ -1,17 +1,54 @@
 package com.jhlee.cleanarchitecture
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
+interface Skill {
+    fun useSkill(): String
+}
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+class ThunderSkill : Skill {
+    override fun useSkill() = "백만볼트~~~~"
+}
+
+class FireSkill : Skill {
+    override fun useSkill(): String {
+        return "받아랏 불대문자!"
+    }
+}
+
+class Pokemon(private val skill: Skill) {
+    fun useSkill(): String {
+        return skill.useSkill()
+    }
+}
+class Pikachu {
+    private val skill: Skill
+    constructor() {
+        this.skill = ThunderSkill()
+    }
+    fun useSkill(): String {
+        return skill.useSkill()
+    }
+}
+
+
 class ExampleUnitTest {
+
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+        val thunderSkill = ThunderSkill()
+        val fireSkill = FireSkill()
+
+        val pikachu = Pokemon(thunderSkill)
+        val charizard = Pokemon(fireSkill)
+
+        val p = pikachu.useSkill()    // 출력: Using Thunder skill!
+        val c = charizard.useSkill()  // 출력: Using Fire skill!
+        assertEquals("Using Thunder skill!", p)
+
+        assertEquals("Using Fire skill!", c)
     }
 }
