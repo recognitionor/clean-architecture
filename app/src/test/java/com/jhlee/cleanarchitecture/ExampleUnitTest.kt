57795fd2d4d565dd2cc1611713306f3fd87e0d59
junghee <junghee@nhn.com>
2023-07-03 20:09:50 +0900

    DI 패턴 예제

--- app/src/test/java/com/jhlee/cleanarchitecture/ExampleUnitTest.kt
+++ app/src/test/java/com/jhlee/cleanarchitecture/ExampleUnitTest.kt
@@ -3,28 +3,40 @@ package com.jhlee.cleanarchitecture
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
+/* 스킬 인터페이스 정의
+    기술을 사용하는 함수
+*/
 interface Skill {
     fun useSkill(): String
 }
 
+// 스킬을 상속받아 번개 스킬 사용하는 클래스 구현
 class ThunderSkill : Skill {
     override fun useSkill() = "백만볼트~~~~"
 }
 
+// 스킬을 상속받아 불 스킬 사용하는 클래스 구현
 class FireSkill : Skill {
     override fun useSkill(): String {
         return "받아랏 불대문자!"
     }
 }
+// 포켓몬 클래스를 생성하는데 생성자로 스킬 객체를 전달 받아
+// useSkil 에서 전달 받은 객체를 통해 기술 발동
 
+// DI 패턴을 사용
 class Pokemon(private val skill: Skill) {
     fun useSkill(): String {
         return skill.useSkill()
     }
 }
-class Pikachu {
+
+// DI 패턴이 아닌 피카추 클래스 내에서 번개스킬을 갖고 있는경우
+// 피카추가 타입이 불로 변경 된경우 대대적인 코드의 수정이 들어가야한다.
+class Pikachu() {
     private val skill: Skill
-    constructor() {
+
+    init {
         this.skill = ThunderSkill()
     }
     fun useSkill(): String {
