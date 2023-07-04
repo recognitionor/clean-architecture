88cf3fff2f72c25d6ee16c55e18d7490f69b2a68
junghee <junghee@nhn.com>
2023-07-04 15:05:53 +0900

    [clean_live_coding] default

--- app/src/main/java/com/jhlee/cleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/jhlee/cleanarchitecture/MainActivity.kt
@@ -2,10 +2,21 @@ package com.jhlee.cleanarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.jhlee.cleanarchitecture.presentation.CoinViewModel
+import com.jhlee.cleanarchitecture.presentation.ui.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen()
+        }
     }
 }
