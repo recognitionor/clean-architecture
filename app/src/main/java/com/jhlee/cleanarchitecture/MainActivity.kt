71aaa69a68555c640c4f8439d5a85686dc3c9162
junghee <junghee@nhn.com>
2023-07-04 19:39:54 +0900

    [clean_live_coding] default

--- app/src/main/java/com/jhlee/cleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/jhlee/cleanarchitecture/MainActivity.kt
@@ -9,11 +9,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MainScreen()
-        }
     }
 }
